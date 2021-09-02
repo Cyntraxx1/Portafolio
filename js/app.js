53caad32745c68d2ffdf78d4259d7b9d157d5b08
estebansubiabre1@gmail.com <estebansubiabre1@gmail.com>
2021-09-01 20:53:05 -0400

    Actualización

--- js/app.js
+++ js/app.js
@@ -7,4 +7,32 @@ function toggleMenu() {
 }
 
 openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu)
+
+const menuLinks = document.querySelectorAll(' .menu a[href^="#"]');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        const id = entry.target.getAttribute("id");
+        const menuLink = document.querySelector(` .menu a[href="#${id}"] `)
+
+        if (entry.isIntersecting) {
+            menuLink.classList.add("selected");
+        } else {
+            menuLink.classList.remove("selected");
+        }
+    })
+})
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function() {
+        menu.classList.remove("menu_opened");
+    })
+
+    const hash =menuLink.getAttribute("href");
+    const target = document.querySelector(hash);
+    if (target {
+        observer.observe(target,);
+    })
+})
+
